d346791a84fc71feded9c87bcd396115b1d26089
Radur1112 <ahernandez20910@gmail.com>
2023-07-11 06:39:30 -0600

    xd

--- client/appCubestore/src/app/producto/producto-routing.module.ts
+++ client/appCubestore/src/app/producto/producto-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoIndexComponent } from './producto-index/producto-index.component';
+import { ProductoAllComponent } from './producto-all/producto-all.component';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'producto', component: ProductoIndexComponent},
+
+  {path:'producto/all', component: ProductoAllComponent},
+
+  {path:'producto/:id', component: ProductoDetailComponent},
+
+  {path:'producto/all/:id', component: ProductoAllComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
